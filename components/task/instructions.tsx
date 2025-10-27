62856a6a121e9bc46d1a974f5068c1b486fcb89b
RyotaNoseyama <r.noseyama@gmail.com>
2025-10-27 16:48:57 +0900

    指示文の修正

--- components/task/instructions.tsx
+++ components/task/instructions.tsx
@@ -10,12 +10,22 @@ export function Instructions() {
       </CardHeader>
       <CardContent className="space-y-2 text-slate-700 leading-relaxed">
         <p>
-          Please write a <strong>caption</strong> for the image below. In this caption, describe the <strong>positional relationships and detailed situations</strong> logically and carefully so that <strong>visually impaired people</strong> can specifically imagine the content of the image. You cannot submit unless it is <strong>at least 30 characters long</strong>.
+          Please write a <strong>caption</strong> for the image below. In this
+          caption, describe the{" "}
+          <strong>positional relationships and detailed situations</strong>{" "}
+          logically and carefully so that{" "}
+          <strong>visually impaired people</strong> can specifically imagine the
+          content of the image. You cannot submit unless it is{" "}
+          <strong>at least 30 characters long</strong>.
         </p>
         <p>
-          This experiment will be conducted continuously for <strong>one week</strong>. Tasks will be published daily from <strong>9 AM to 9 PM America time</strong>.
+          This experiment will be conducted continuously for{" "}
+          <strong>one week</strong>. Tasks will be published daily from{" "}
+          <strong>9 AM to 9 PM America time</strong>.
         </p>
-        <p><strong>Avoid personal data and offensive language.</strong></p>
+        <p>
+          <strong>Avoid personal data and offensive language.</strong>
+        </p>
       </CardContent>
     </Card>
   );
