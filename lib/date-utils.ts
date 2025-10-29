3381875cea90d467413c459ff5a005c9edf19975
RyotaNoseyama <r.noseyama@gmail.com>
2025-10-29 15:57:09 +0900

    コード生成修正

--- lib/date-utils.ts
+++ lib/date-utils.ts
@@ -75,9 +75,9 @@ export function generateCompletionCode(): string {
     })
     .join("-");
 
-  // 真ん中の文字（7文字目、ハイフンを含めると8文字目）を'R'に固定
+  // 真ん中の文字（7文字目、ハイフンを含めると8文字目）を'O'に固定
   const codeArray = code.split("");
-  codeArray[6] = "Y"; // XXXX-XXRX-XXXX の R
+  codeArray[6] = "O"; // XXXX-XXOX-XXXX の O
 
   return `COMP-${codeArray.join("")}`;
 }
